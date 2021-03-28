2d6eb819029fc45c46ce2e963f741ffce2977d68
Ortex58 <ortex@i.ua>
2021-03-28 16:58:41 +0300

    second commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,23 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
-
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+// import Navbar from bootstrap
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
+require('~/node_modules/bootstrap/dist/js/bootstrap.min.js')
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
+	$('.burger').on('click', function() {
+		if ( !$('.navbar-collapse').hasClass('show') ) {
+			$('.navbar').addClass('fixed-top');
+			$('body').addClass('overflow-hidden');
+		} else {
+			$('.navbar').removeClass('fixed-top');
+			$('body').removeClass('overflow-hidden');
+		}
+	});
 
 })
